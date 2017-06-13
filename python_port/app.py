dc649e24ea9996dced0799b1a312e9c0308bc78c
Nassan Paul <nassanpaul@gmail.com>
2017-06-13 22:50:29 +0300

    worked saved

--- python_port/app.py
+++ python_port/app.py
@@ -1,33 +1,39 @@
 """An embed server for Sefaria.org"""
+import sys
 from threading import Thread
 from urlparse import urljoin
 from time import time
-import persistent
-from BTrees.OOBTree import BTree
 
-import ZODB
-import ZODB.FileStorage
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
 from flask import Flask
 from flask import render_template
 
 from constants import *
 import requests
 
+
 #Database
-storage = ZODB.FileStorage.FileStorage('data.fs')
-db = ZODB.DB(storage)
+engine = create_engine('sqlite:///test.db', echo=False)
+Session = sessionmaker(bind=engine)
+# Or do:
+# Session = sessionmaker()
+# Session.configure(bind=engine)  # once engine is available
 
-def init_db():
-    """Makes sure the database is in the proper state"""
-    with db.transaction() as conn:
-        dbroot = conn.root()
-        if "resources" in dbroot:
-            return
-        else:
-            dbroot["resources"] = {}
-        return
+session = Session()
 
-init_db()
+Base = declarative_base()
+Base.metadata.create_all(engine)
+
+class SefariaResource(Base):
+    __tablename__ = 'sefaria_resources'
+    id = Column(Integer, primary_key=True)
+    HebrewText = Column(String)
+    EnglishText = Column(String)
+    HebrewSectionReference = Column(String)
+    EnglishSectionReference = Column(String)
 
 # Web App
 app = Flask(__name__)
@@ -35,25 +41,25 @@ app = Flask(__name__)
 @app.route("/<resource>")
 def root(resource):
     """Returns the embed page for a given Sefaria resource"""
-    result = get_resource(resource)
-
-    result["defaultLanguageCode"] = "he"
-    result["originalURLPath"] = resource
-    return render_template("embed.j2", ob=result)
+    try:
+        # result = get_resource(resource)
+        # return result
+        return "Hello"
+    except:
+        e = sys.exc_info()[0]
+        print e
+        return "There was an Error" 
+    # result["defaultLanguageCode"] = "he"
+    # result["originalURLPath"] = resource
+    # return render_template("embed.j2", ob=result)
 
 
 def get_resource(resource_name):
     """Looks for the Sefaria resource locally, otherwise fetches it from the api"""
     #Check if it exists locally
-    with db.transaction() as conn:
-        dbroot = conn.root()
-        if resource_name in dbroot["resources"]:
-            result = dbroot["resources"][resource_name]
-            print "Found result locally\t" + result["HebrewSectionReference"]
-            
-            # Update the last-touched timestamp to prevent it being cleared from the cache
-            result["last_touched"] = time()
-            return result
+    # result = local_get_resource("fake_resource")
+
+    # return result
 
     #If it does not exist locally, get it from the api
     result = fetch_resource(resource_name)
@@ -62,32 +68,40 @@ def get_resource(resource_name):
 # Remote resource function
 def fetch_resource(resource_name):
     """Issues a GET request to fetch resource and returns dictionary of the relevant data"""
-    response = requests.get(urljoin(SEFARIA_API_NODE, resource_name))
-    full_json = response.json()
+    # url = urljoin(SEFARIA_API_NODE, resource_name)
+    print resource_name
+    # response = requests.get(url)
+    # response = requests.get(urljoin(SEFARIA_API_NODE, resource_name))
+    # response.encoding = "UTF-8"
+    # full_json = response.json()
 
     # Parse the relevant parts of the json
     parsed_response = {}
-    parsed_response["HebrewText"] = full_json["he"]
-    parsed_response["EnglishText"] = full_json["text"]
-    parsed_response["HebrewSectionReference"] = full_json["heSectionRef"]
-    parsed_response["EnglishSectionReference"] = full_json["sectionRef"]
-    parsed_response["last_touched"] = time()
-
-    print "Found result remotely\t" + parsed_response["HebrewSectionReference"]
-
-    # Save it locally for the future
-    with db.transaction() as conn:
-        dbroot = conn.root()
-        dbroot["resources"][resource_name] = parsed_response
+    # parsed_response["HebrewText"] = full_json["he"]
+    # parsed_response["EnglishText"] = full_json["text"]
+    # parsed_response["HebrewSectionReference"] = full_json["heSectionRef"]
+    # parsed_response["EnglishSectionReference"] = full_json["sectionRef"]
 
     return parsed_response
 
-class Resources(persistent.Persistent):
-    def __init__(self):
-        self.resources = BTree()
-    
-    def addResource(self, resource_name, resource):
-        self.resources.insert(resource_name, resource)
+def local_get_resource(resource_hebrew_section_reference):
+    print resource_hebrew_section_reference
+    resource = session.query(SefariaResource).filter_by(HebrewSectionReference=resource_hebrew_section_reference).first()
+    print "Resource: "+ resource
+    return resource
+def local_add_resource(resource_dict):
+    resource = SefariaResource(
+        HebrewText=resource_dict["HebrewText"],
+        EnglishText=resource_dict["EnglishText"],
+        HebrewSectionReference=resource_dict["HebrewSectionReference"],
+        EnglishSectionReference=resource_dict["EnglishSectionReference"])
+
+    session.add(resource)
+    session.commit()
+    # HebrewText = Column(String)
+    # EnglishText = Column(String)
+    # HebrewSectionReference = Column(String)
+    # EnglishSectionReference = Column(String)
 
 if __name__ == "__main__":
     app.run(port=3017)
