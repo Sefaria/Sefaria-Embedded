27705be844ad8a8f8af47c442c9f1702fbef9c7e
Nassan Paul <nassanpaul@gmail.com>
2017-06-28 16:02:50 +0300

    template and app prepared for rich link previews

--- python_port/app.py
+++ python_port/app.py
@@ -12,10 +12,7 @@ from sqlalchemy.schema import ForeignKey
 from sqlalchemy.orm import sessionmaker, scoped_session
 from sqlalchemy.ext.declarative import declarative_base
 
-from flask import Flask
-from flask import render_template
-
-
+from flask import Flask, render_template, request
 
 from constants import *
 import requests
@@ -38,6 +35,7 @@ class SefariaResource(Base):
     id = Column(Integer, primary_key=True)
     HebrewSectionReference = Column(String)
     EnglishSectionReference = Column(String)
+    resource_name = Column(String)
     seen_count = Column(Integer, default=1)
     created_timestamp = Column(DateTime, server_default=func.now())
     last_seen_timestamp = Column(DateTime, server_default=func.now(), onupdate=func.now())
@@ -62,7 +60,17 @@ def root(resource):
     result = get_resource(resource)
     render_obj = {}
 
-    render_obj["defaultLanguageCode"] = "he"
+    lang = request.args.get('lang')
+    if lang == 'he':
+        render_obj["defaultLanguageCode"] = "he"
+        render_obj["meta_description"] = get_first_50_characters(result["HebrewText"])
+    elif lang == 'en':
+        render_obj["defaultLanguageCode"] = "en"
+        render_obj["meta_description"] = get_first_50_characters(result["EnglishText"])
+    else:
+        render_obj["defaultLanguageCode"] = "en"
+        render_obj["meta_description"] = get_first_50_characters(result["EnglishText"])
+
     render_obj["originalURLPath"] = resource
     render_obj["data"] = result
 
@@ -99,7 +107,8 @@ def remote_get_resource(resource_name):
     parsed_response["HebrewText"] = full_json["he"]
     parsed_response["EnglishText"] = full_json["text"]
     parsed_response["HebrewSectionReference"] = full_json["heSectionRef"]
-    parsed_response["EnglishSectionReference"] = resource_name
+    parsed_response["EnglishSectionReference"] = full_json["sectionRef"]
+    parsed_response["resource_name"] = resource_name
 
     # Add the resource to the local data store
     local_add_resource(parsed_response)
@@ -107,8 +116,8 @@ def remote_get_resource(resource_name):
     # Pass the resource back to the web app
     return parsed_response
 
-def local_get_resource(resource_hebrew_section_reference):
-    resource = session.query(SefariaResource).filter_by(EnglishSectionReference=resource_hebrew_section_reference).first()
+def local_get_resource(resource_name):
+    resource = session.query(SefariaResource).filter_by(resource_name=resource_name).first()
 
     if resource is None:
         return resource
@@ -135,7 +144,10 @@ def local_get_resource(resource_hebrew_section_reference):
     return result
 
 def local_add_resource(resource_dict):
-    resource = SefariaResource(HebrewSectionReference=resource_dict["HebrewSectionReference"], EnglishSectionReference=resource_dict["EnglishSectionReference"])
+    resource = SefariaResource(
+        HebrewSectionReference=resource_dict["HebrewSectionReference"],
+        EnglishSectionReference=resource_dict["EnglishSectionReference"],
+        resource_name=resource_dict["resource_name"])
     session.add(resource)
     session.flush() #To make sure the id is assigned for future foreign key reference
 
@@ -170,12 +182,12 @@ def local_monitor_resources(delay):
         for resource in session2.query(SefariaResource):
             seconds_old = (datetime.utcnow() - resource.last_seen_timestamp).total_seconds()
             if seconds_old > CACHE_LIFETIME_SECONDS:
-                print "Deleting " + resource.EnglishSectionReference + " which is " + str(seconds_old) + " seconds old"
-                local_delete_resource(session2, resource.EnglishSectionReference)
+                print "Deleting " + resource.resource_name + " which is " + str(seconds_old) + " seconds old"
+                local_delete_resource(session2, resource.resource_name)
         time.sleep(delay)
 
-def local_delete_resource(session2, resource_english_section_reference):
-    resource = session2.query(SefariaResource).filter_by(EnglishSectionReference=resource_english_section_reference).first()
+def local_delete_resource(session2, resource_name):
+    resource = session2.query(SefariaResource).filter_by(resource_name=resource_name).first()
 
     if resource is None:
         return resource
@@ -190,6 +202,14 @@ def local_delete_resource(session2, resource_english_section_reference):
 
     session2.commit()
 
+def get_first_50_characters(array):
+    result = ""
+    for string in array:
+        for char in string:
+            result += char
+            if len(result) >= 50:
+                return result
+    return result
 
 if __name__ == "__main__":
     Thread(target=local_monitor_resources, args=(CACHE_MONITOR_LOOP_DELAY_IN_SECONDS,)).start()
