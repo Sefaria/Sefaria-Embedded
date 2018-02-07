9e3569baad89fe4e6610ae9fba192493cca2b182
Russel Neiss <russel.neiss@gmail.com>
2018-02-07 15:26:42 -0600

    Change ports - temporarily disable threading

--- app.py
+++ app.py
@@ -406,5 +406,5 @@ def cleanup_and_format_text(text, language):
 
 
 if __name__ == "__main__":
-    Thread(target=local_monitor_resources, args=(CACHE_MONITOR_LOOP_DELAY_IN_SECONDS,)).start()
-    app.run(host='0.0.0.0', port=443)
+#    Thread(target=local_monitor_resources, args=(CACHE_MONITOR_LOOP_DELAY_IN_SECONDS,)).start()
+    app.run(host='0.0.0.0', port=5000)
